cba04867da2aea07dd660b08aec21d62f988e7ae
emilyshute <emilyshute116@gmail.com>
2020-10-04 14:34:33 +0800

    top 10 otus

--- app.js
+++ app.js
@@ -2,4 +2,13 @@
 var url = "Users/matthewvicario/Desktop/Plot.ly-Challenge/Data/samples.json"
 d3.json(url).then(sampleData) => {
     console.log(sampleData)
+    
+    var IDs = sampleData.samples[0].otu_IDs;
+    console.log(ids)
+
+    var values = sampleData.samples[0]sample_values.slice(0,10).reverse();
+    console.log(values)
+
+    var labels = sampleData.sample[0].otu_labels.slice(0,10);
+    console.log(labels)
 }
